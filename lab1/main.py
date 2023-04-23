420abc48f2ace187517c738ed15cb6d6cf896834
Kamil Chaj <kamilchaj@student.agh.edu.pl>
2023-04-23 20:05:21 +0200

    we got this

--- lab1/main.py
+++ lab1/main.py
@@ -1,26 +1,41 @@
 import numpy as np
 import cmath as cm
 
-f = 1000
-Zc11 = 1/complex(0, f*100e-9)
-Zc12 = 1/complex(0, f*47e-9)
-Zc13 = 1/complex(0, f*22e-9)
-Zr11 = complex(1e3)
-Zr12 = complex(1e3)
-Zl11 = complex(0, f*10e-3)
+f=1e3
+w=2*3.14*f
+Zr=1e3
+Zc1=1/complex(0,-((47e-9)*w))
+Zc2=1/complex(0,-((100e-9)*w))
+Zc3=1/complex(0,-((100e-9)*w))
+Zl=complex(0,(10e-3)*w)
+V = 1.117
 
-v = 1.17
+G2 = 2/Zr + 1/Zc1
+G3 = 1/Zc1 + 1/Zc2 + 1/Zl
+G4 = 1/Zc3 + 1/Zl + 1/Zr
+G23 = -1/Zc1
+G24 = -1/Zr
+G34 = -1/Zl
 
-G = np.array([[1/Zc11 + 1/Zr11 + 1/Zc13, -1/Zr12],
-              [-1/Zr12, 1/Zc12 + 1/Zl11]])
-I = np.array([[v/(Zr11+Zc11)],
-              [v/Zc12]])
+# Ga = complex(0.002, 2.963e-4)
+# Gb = complex(0.001, 0.01654)
+# Gab = 1/1000
 
-U = np.matmul(np.linalg.inv(G), I)
+I1 = 0
+I2 = V / (Zc2)
+I3 = 0
+G = np.array([[G2, G23, G24],
+              [G23, G3, G34],
+              [G24, G34, G4]])
+I = np.array([[I1],
+              [I2],
+              [I3]])
 
-Um = np.sqrt(pow(np.real(U), 2) + pow(np.imag(U), 2))
+print('G: \n', G)
+print('invG: \n', np.linalg.inv(G))
+print('I: \n', I)
 
-print('I: \n',I)
-print('G: \n',G)
-print('U: \n',U)
-print('|U|: \n', Um)
+V = np.matmul(np.linalg.inv(G), I)
+
+print('V: \n', V)
+print('V: \n', np.abs(V))
