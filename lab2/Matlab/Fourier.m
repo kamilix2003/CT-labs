4db6bc15273f0dc9e893b39156c7df7e4be9eba6
Kamil Chaj <kamilchaj@student.agh.edu.pl>
2023-05-12 20:55:04 +0200

    pure signals and fourier coeff

--- lab2/Matlab/Fourier.m
+++ lab2/Matlab/Fourier.m
@@ -4,29 +4,34 @@ clear all; close all; clc;
 frequency = 1000;
 Vpp = 5;
 L = 1/frequency;
-N = 1024;
+N = 2048;
 dx = 2*L/(N-1);
 x = -L:dx:L;
 
-%functions
+%%signals
+%sin
 Sinus = 0*x;
 Sinus(1:N) = sin(2*pi*frequency*x);
+%square duty cycle 50
 Square50 = 0*x;
 Square50(1:N/2-1) = -Vpp/2;
 Square50(N/2:N) = Vpp/2;
+%square duty cycle 25
 Square25 = 0*x;
 Square25(1:N/4-1) = -Vpp/2;
 Square25(N/4:N) = Vpp/2;
+%triangle symetry ratio 50
 Triangle50 = 0*x;
 Triangle50(1:N/2-1) = Vpp*(1:N/2-1)/N-(Vpp/2);
 Triangle50(N/2:N) = -Vpp*(N/2:N)/N+(Vpp/2);
+%triangle symetry ratio 40
 Triangle40 = 0*x;
 Triangle40(1:(4*N/10-1)) = (Vpp/0.4*(1:4*N/10-1))/N-(Vpp*0.5);
-Triangle40((4*N/10):N) = -(Vpp/0.6*(4*N/10:N))/N+(Vpp*0.8);
+Triangle40((4*N/10):N) = -(Vpp/0.6*(4*N/10:N))/N+(Vpp*1.167);
 
 %fourier coefficient
-[A, B] = fourier_coefficient(Sinus);
-fourier_coefficient(Square50);
-fourier_coefficient(Square25);
-fourier_coefficient(Triangle50);
-fourier_coefficient(Triangle40);
+fourier_coefficient(Sinus, frequency, N, 'sinus');
+fourier_coefficient(Square50, frequency, N, 'square wave 50% duty cycle');
+fourier_coefficient(Square25, frequency, N, 'square wave 25% duty cycle');
+fourier_coefficient(Triangle50, frequency, N, 'triangle wave 50% symmetry ratio');
+fourier_coefficient(Triangle40, frequency, N, 'triangle wave 40% symmetry ratio');
---
+++ lab2/Matlab/RC_circuit.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = RC_circuit(inputArg1,inputArg2)
+%RC_CIRCUIT Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
