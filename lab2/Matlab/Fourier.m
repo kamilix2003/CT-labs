d5384460b9f7616ba1f849189eaca9a01fb08397
Kamil Chaj <kamilchaj@student.agh.edu.pl>
2023-05-16 23:29:35 +0200

    RC filters

--- lab2/Matlab/Fourier.m
+++ lab2/Matlab/Fourier.m
@@ -29,22 +29,16 @@ Triangle40 = 0*x;
 Triangle40(1:(4*N/10-1)) = (Vpp/0.4*(1:4*N/10-1))/N-(Vpp*0.5);
 Triangle40((4*N/10):N) = -(Vpp/0.6*(4*N/10:N))/N+(Vpp*1.167);
 %%RC filters responses
-% [S5I, S5D, T4I, T4D, time] = Import_csv('csv/RC_filters_1.csv');
-% figure(1);
-% plot(time, S5I);
-% figure(2)
-% plot(time, S5D);
-% figure(3)
-% plot(time, T4I);
-% figure(4)
-% plot(time, T4D);
+[Square50_integrator, Square50_differentiatior, Triangle40_integrator, Triangle40_differentiatior, time] = Import_csv('csv/RC_filters.csv');
 
 pause(1);
 %fourier coefficient
-fourier_coefficient(Sinus, frequency, 'sinus', 'sin');
-fourier_coefficient(Square50, frequency, 'square wave 50% duty cycle', 'sqr50');
-fourier_coefficient(Square25, frequency, 'square wave 25% duty cycle', 'sqr25');
-fourier_coefficient(Triangle50, frequency, 'triangle wave 50% symmetry ratio', 'tri50');
-fourier_coefficient(Triangle40, frequency, 'triangle wave 40% symmetry ratio', 'tri40');
-% [test] = RC_circuit(frequency, N);
-% fourier_coefficient(test,frequency, N, 'test', 'test');
+% fourier_coefficient(Sinus, frequency, 'sinus', 'sin');
+% fourier_coefficient(Square50, frequency, 'square wave 50% duty cycle', 'sqr50');
+% fourier_coefficient(Square25, frequency, 'square wave 25% duty cycle', 'sqr25');
+% fourier_coefficient(Triangle50, frequency, 'triangle wave 50% symmetry ratio', 'tri50');
+% fourier_coefficient(Triangle40, frequency, 'triangle wave 40% symmetry ratio', 'tri40');
+fourier_coefficient(Square50_integrator, frequency, 'RC low-pass filter square wave 50% duty cycle', 'RCLPsqr50');
+fourier_coefficient(Square50_differentiatior, frequency, 'RC high-pass filter square wave 50% duty cycle', 'RCHPsqr50');
+fourier_coefficient(Triangle40_integrator, frequency, 'RC low-pass filter triangle wave 40% symmetry ratio', 'RCLPtri40');
+fourier_coefficient(Triangle40_differentiatior, frequency, 'RC high-pass filter triangle wave 40% symmetry ratio', 'RCHPtri40');
--- lab2/Matlab/Import_csv.m
+++ lab2/Matlab/Import_csv.m
@@ -1,10 +1,10 @@
 function [Square50_integrator, Square50_differentiatior, Triangle40_integrator, Triangle40_differentiatior, time] = Import_csv(filepath)
 CSV = readtable(filepath);
 % CSV = rows2vars(CSV);
-Triangle40_integrator = table2array(CSV(:,2));
-Triangle40_differentiatior= table2array(CSV(:,5));
-Square50_integrator = table2array(CSV(:,8));
-Square50_differentiatior = table2array(CSV(:,11));
-time = table2array(CSV(:,1));
+Triangle40_integrator = table2array(CSV(10032:20064,2))';
+Triangle40_differentiatior= table2array(CSV(10032:20064,5))';
+Square50_integrator = table2array(CSV(10032:20064,8))';
+Square50_differentiatior = table2array(CSV(10032:20064,11))';
+time = table2array(CSV(10032:20064,1))';
 end
 
