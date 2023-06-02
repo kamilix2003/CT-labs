ce58749abe0dcf5275975953dc8065a39f3f030a
Kamil Chaj <kamaj2309@gmail.com>
2023-06-02 22:40:48 +0200

    some laplace

--- lab3/Matlab/main.m
+++ lab3/Matlab/main.m
@@ -19,33 +19,33 @@ t = step:step:T;
 x = response1(t, 1.5e3, 47e-9);
 figure('Name','Circuit A')
 plot(t, x)
-ylim([-0.2, 2])
-xlim([0.45*T, 0.6*T])
+ylim([offset-0.2, amplitude+offset+0.2])
+xlim([0.48*T, 0.6*T])
 
-%% Circuit B
-syms C2 R response2(t, R, C2)
-response2(t, R, C2) = ilaplace(laplace(square) * R/(R + 1/(s*C2)));
-steps = 1000;
-step = T/steps;
-t = step:step:T;
-x = response2(t, 1.5e3, 10e-9);
-figure('Name','Circuit B')
-plot(t, x)
-ylim([-0.2, 2])
-
-%% Circuit C
-syms R1 R2 C1 L1 response33(t, C1, L1) response32(t, C1, L1, R2) response(t, C1, L1, R1)
-response33(t, C1 , L1) = ilaplace(laplace(square) * (s*L1)/(s*L1 + 1/(s*C1)))
-response32(t, C1, L1, R2) = ilaplace(laplace(square) * (s*L1)/(s*L1 + 1/(s*C1) + R2))
-steps = 1000;
-step = T/steps;
-t = step:step:T;
-x = response33(t, 33e3, 10e-3);
-x32 = response32(t, 33e3, 10e-3, 1.1e3)
-figure('Name','Circuit C33')
-plot(t, x); hold on;
-plot(t, x32)
-ylim([-0.2, 2])
-
+% %% Circuit B
+% syms C2 R response2(t, R, C2)
+% response2(t, R, C2) = ilaplace(laplace(square) * R/(R + 1/(s*C2)));
+% steps = 1000;
+% step = T/steps;
+% t = step:step:T;
+% x = response2(t, 1.5e3, 10e-9);
+% figure('Name','Circuit B')
+% plot(t, x)
+% ylim([-0.2, 2])
+% 
+% %% Circuit C
+% syms R1 R2 C1 L1 response33(t, C1, L1) response32(t, C1, L1, R2) response(t, C1, L1, R1)
+% response33(t, C1 , L1) = ilaplace(laplace(square) * (s*L1)/(s*L1 + 1/(s*C1)))
+% response32(t, C1, L1, R2) = ilaplace(laplace(square) * (s*L1)/(s*L1 + 1/(s*C1) + R2))
+% steps = 1000;
+% step = T/steps;
+% t = step:step:T;
+% x = response33(t, 33e3, 10e-3);
+% x32 = response32(t, 33e3, 10e-3, 1.1e3)
+% figure('Name','Circuit C33')
+% plot(t, x); hold on;
+% plot(t, x32)
+% ylim([-0.2, 2])
+% 
 
 
